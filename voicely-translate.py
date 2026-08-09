4fb3790058f45592827cf88dd392f194ab5eeda4
Erallie <eds.gozar@outlook.com>
2026-08-08 22:34:03 -0700

    Adjusted the non-speech filter

--- voicely-translate.py
+++ voicely-translate.py
@@ -46,8 +46,8 @@ MAX_UTTERANCE_SECONDS = 25.0
 # WebRTC voice activity detection. 0 is least aggressive, 3 is most aggressive.
 VAD_AGGRESSIVENESS = 3
 VAD_FRAME_MS = 30
-VAD_MIN_SPEECH_RATIO = 0.35
-VAD_MIN_CONSECUTIVE_SPEECH_FRAMES = 4
+VAD_MIN_SPEECH_RATIO = 0.40
+VAD_MIN_CONSECUTIVE_SPEECH_FRAMES = 6
 
 # How frequently the bot checks whether an utterance has ended.
 BUFFER_CHECK_INTERVAL = 0.10
